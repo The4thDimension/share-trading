6ac96ddd0383e4c5f192577e5119f484ff34be8e
Mahesh <pmaheshiyer@gmail.com>
2025-05-05 08:57:18 +0530

    Remove Print

--- dataCollection.py
+++ dataCollection.py
@@ -25,7 +25,6 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-print(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
 
 def send_telegram_alert(message):
     try:
